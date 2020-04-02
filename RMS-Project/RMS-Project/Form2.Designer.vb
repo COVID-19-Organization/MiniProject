53cdb61e60c6a31063fddcfc559dd69b519902ae
NUTCHA KAEWJAN <6110210121@email.psu.ac.th>
2020-04-02 23:13:28 +0700

    sum

--- RMS-Project/RMS-Project/Form2.Designer.vb
+++ RMS-Project/RMS-Project/Form2.Designer.vb
@@ -22,9 +22,317 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.txt_Add_Type = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_Add_Name = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txt_Add_ID = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btn_Select_Pic = New System.Windows.Forms.Button()
+        Me.pic_Add = New System.Windows.Forms.PictureBox()
+        Me.txt_Add_Price2 = New System.Windows.Forms.TextBox()
+        Me.txt_Add_Price = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGrid_Showmenu = New System.Windows.Forms.DataGridView()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.pic_Add, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        CType(Me.DataGrid_Showmenu, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 32)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(476, 535)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "ลบรายการอาหาร"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'txt_Add_Type
+        '
+        Me.txt_Add_Type.Location = New System.Drawing.Point(155, 89)
+        Me.txt_Add_Type.Name = "txt_Add_Type"
+        Me.txt_Add_Type.Size = New System.Drawing.Size(183, 34)
+        Me.txt_Add_Type.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(24, 92)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(104, 27)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "ประเภทอาหาร"
+        '
+        'txt_Add_Name
+        '
+        Me.txt_Add_Name.Location = New System.Drawing.Point(155, 50)
+        Me.txt_Add_Name.Name = "txt_Add_Name"
+        Me.txt_Add_Name.Size = New System.Drawing.Size(183, 34)
+        Me.txt_Add_Name.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 57)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 27)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "ชื่ออาหาร"
+        '
+        'txt_Add_ID
+        '
+        Me.txt_Add_ID.Location = New System.Drawing.Point(156, 12)
+        Me.txt_Add_ID.Name = "txt_Add_ID"
+        Me.txt_Add_ID.Size = New System.Drawing.Size(183, 34)
+        Me.txt_Add_ID.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 27)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "รหัส"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 32)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(476, 535)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "แก้ไขรายการอาหาร"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(206, 76)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 27)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "บาท"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(206, 36)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 27)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "บาท"
+        '
+        'btn_Select_Pic
+        '
+        Me.btn_Select_Pic.Location = New System.Drawing.Point(360, 329)
+        Me.btn_Select_Pic.Name = "btn_Select_Pic"
+        Me.btn_Select_Pic.Size = New System.Drawing.Size(99, 34)
+        Me.btn_Select_Pic.TabIndex = 8
+        Me.btn_Select_Pic.Text = "เลือกรูปภาพ"
+        Me.btn_Select_Pic.UseVisualStyleBackColor = True
+        '
+        'pic_Add
+        '
+        Me.pic_Add.Location = New System.Drawing.Point(29, 262)
+        Me.pic_Add.Name = "pic_Add"
+        Me.pic_Add.Size = New System.Drawing.Size(310, 188)
+        Me.pic_Add.TabIndex = 7
+        Me.pic_Add.TabStop = False
+        '
+        'txt_Add_Price2
+        '
+        Me.txt_Add_Price2.Location = New System.Drawing.Point(120, 73)
+        Me.txt_Add_Price2.Name = "txt_Add_Price2"
+        Me.txt_Add_Price2.Size = New System.Drawing.Size(80, 34)
+        Me.txt_Add_Price2.TabIndex = 3
+        '
+        'txt_Add_Price
+        '
+        Me.txt_Add_Price.Location = New System.Drawing.Point(120, 33)
+        Me.txt_Add_Price.Name = "txt_Add_Price"
+        Me.txt_Add_Price.Size = New System.Drawing.Size(80, 34)
+        Me.txt_Add_Price.TabIndex = 2
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(37, 76)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 27)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "พิเศษ"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(37, 40)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 27)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "ธรรมดา"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(100, 474)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(101, 45)
+        Me.btnSave.TabIndex = 9
+        Me.btnSave.Text = "บันทึก"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.btnSave)
+        Me.TabPage1.Controls.Add(Me.btn_Select_Pic)
+        Me.TabPage1.Controls.Add(Me.pic_Add)
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
+        Me.TabPage1.Controls.Add(Me.txt_Add_Type)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.txt_Add_Name)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.txt_Add_ID)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Font = New System.Drawing.Font("TH Sarabun New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabPage1.Location = New System.Drawing.Point(4, 32)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(476, 535)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "เพิ่มรายการอาหาร"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.txt_Add_Price2)
+        Me.GroupBox1.Controls.Add(Me.txt_Add_Price)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Location = New System.Drawing.Point(29, 126)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(310, 127)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "ราคาอาหาร"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Font = New System.Drawing.Font("TH Sarabun New", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabControl1.Location = New System.Drawing.Point(493, 72)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(484, 571)
+        Me.TabControl1.TabIndex = 9
+        Me.TabControl1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Teal
+        Me.Label2.Font = New System.Drawing.Font("TH Sarabun New", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label2.Location = New System.Drawing.Point(-1, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(1072, 57)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "จัดการข้อมูลรายการอาหาร"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'DataGrid_Showmenu
+        '
+        Me.DataGrid_Showmenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGrid_Showmenu.Location = New System.Drawing.Point(24, 116)
+        Me.DataGrid_Showmenu.Name = "DataGrid_Showmenu"
+        Me.DataGrid_Showmenu.RowTemplate.Height = 24
+        Me.DataGrid_Showmenu.Size = New System.Drawing.Size(432, 523)
+        Me.DataGrid_Showmenu.TabIndex = 7
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("TH Sarabun New", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(93, 72)
+        Me.txtSearch.Multiline = True
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(363, 38)
+        Me.txtSearch.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("TH Sarabun New", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label1.Location = New System.Drawing.Point(21, 79)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 32)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Search"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.DarkCyan
+        Me.ClientSize = New System.Drawing.Size(1021, 651)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DataGrid_Showmenu)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
         Me.Text = "Form2"
+        CType(Me.pic_Add, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        CType(Me.DataGrid_Showmenu, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents txt_Add_Type As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_Add_Name As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txt_Add_ID As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btn_Select_Pic As Button
+    Friend WithEvents pic_Add As PictureBox
+    Friend WithEvents txt_Add_Price2 As TextBox
+    Friend WithEvents txt_Add_Price As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnSave As Button
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DataGrid_Showmenu As DataGridView
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents Label1 As Label
 End Class
