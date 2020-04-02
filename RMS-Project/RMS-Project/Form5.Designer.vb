9d86478de0dd5cd1b030cb1bab509425c46ffc57
nung3044 <neungunlove@hotmail.com>
2020-04-02 23:18:12 +0700

    sfsdf

--- RMS-Project/RMS-Project/Form5.Designer.vb
+++ RMS-Project/RMS-Project/Form5.Designer.vb
@@ -35,7 +35,7 @@ Partial Class Form5
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(193, 9)
+        Me.Label1.Location = New System.Drawing.Point(222, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(379, 91)
         Me.Label1.TabIndex = 0
