a3b1c00a5725f7a596fa097c0ed54a662213cc65
P <6110210015@psu.ac.th>
2020-04-06 19:38:54 +0700

    update

--- RMS-Project/RMS-Project/Form4.Designer.vb
+++ RMS-Project/RMS-Project/Form4.Designer.vb
@@ -23,89 +23,34 @@ Partial Class Form4
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.DataSet1 = New RMS_Project.DataSet1()
         Me.TableBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.TableTableAdapter = New RMS_Project.DataSet1TableAdapters.TableTableAdapter()
-        Me.TableidDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.TablenumberDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.TablestatusidDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.DataSet1 = New RMS_Project.DataSet1()
         CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.AutoGenerateColumns = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.TableidDataGridViewTextBoxColumn, Me.TablenumberDataGridViewTextBoxColumn, Me.TablestatusidDataGridViewTextBoxColumn})
-        Me.DataGridView1.DataSource = Me.TableBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(269, 57)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersWidth = 51
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(442, 150)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'DataSet1
-        '
-        Me.DataSet1.DataSetName = "DataSet1"
-        Me.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'TableBindingSource
         '
         Me.TableBindingSource.DataMember = "Table"
         Me.TableBindingSource.DataSource = Me.DataSet1
         '
-        'TableTableAdapter
+        'DataSet1
         '
-        Me.TableTableAdapter.ClearBeforeFill = True
-        '
-        'TableidDataGridViewTextBoxColumn
-        '
-        Me.TableidDataGridViewTextBoxColumn.DataPropertyName = "table_id"
-        Me.TableidDataGridViewTextBoxColumn.HeaderText = "table_id"
-        Me.TableidDataGridViewTextBoxColumn.MinimumWidth = 6
-        Me.TableidDataGridViewTextBoxColumn.Name = "TableidDataGridViewTextBoxColumn"
-        Me.TableidDataGridViewTextBoxColumn.Width = 125
-        '
-        'TablenumberDataGridViewTextBoxColumn
-        '
-        Me.TablenumberDataGridViewTextBoxColumn.DataPropertyName = "table_number"
-        Me.TablenumberDataGridViewTextBoxColumn.HeaderText = "table_number"
-        Me.TablenumberDataGridViewTextBoxColumn.MinimumWidth = 6
-        Me.TablenumberDataGridViewTextBoxColumn.Name = "TablenumberDataGridViewTextBoxColumn"
-        Me.TablenumberDataGridViewTextBoxColumn.Width = 125
-        '
-        'TablestatusidDataGridViewTextBoxColumn
-        '
-        Me.TablestatusidDataGridViewTextBoxColumn.DataPropertyName = "table_status_id"
-        Me.TablestatusidDataGridViewTextBoxColumn.HeaderText = "table_status_id"
-        Me.TablestatusidDataGridViewTextBoxColumn.MinimumWidth = 6
-        Me.TablestatusidDataGridViewTextBoxColumn.Name = "TablestatusidDataGridViewTextBoxColumn"
-        Me.TablestatusidDataGridViewTextBoxColumn.Width = 125
+        Me.DataSet1.DataSetName = "DataSet1"
+        Me.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'Form4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1233, 490)
-        Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form4"
         Me.Text = "ใบเสร็จ"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents DataSet1 As DataSet1
     Friend WithEvents TableBindingSource As BindingSource
-    Friend WithEvents TableTableAdapter As DataSet1TableAdapters.TableTableAdapter
-    Friend WithEvents TableidDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents TablenumberDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents TablestatusidDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 End Class
