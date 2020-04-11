51bff5333583fd077c798f9ffaa94d0b41586940
nung3044 <neungunlove@hotmail.com>
2020-04-11 22:15:09 +0700

    Save db income

--- RMS-Project/RMS-Project/Form1.vb
+++ RMS-Project/RMS-Project/Form1.vb
@@ -212,4 +212,8 @@
             End If
         End If
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form4.Show()
+    End Sub
 End Class
