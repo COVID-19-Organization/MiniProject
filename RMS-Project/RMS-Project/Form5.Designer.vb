170b07a8950246e67a4fcb336050ddd80e15c2e5
nung3044 <neungunlove@hotmail.com>
2020-04-25 01:10:58 +0700

    11111

--- RMS-Project/RMS-Project/Form5.Designer.vb
+++ RMS-Project/RMS-Project/Form5.Designer.vb
@@ -27,50 +27,103 @@ Partial Class Form5
         Me.btn1 = New System.Windows.Forms.Button()
         Me.inprice = New System.Windows.Forms.TextBox()
         Me.DateTimeIncome = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'inID
         '
-        Me.inID.Location = New System.Drawing.Point(67, 58)
+        Me.inID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.inID.Location = New System.Drawing.Point(109, 67)
         Me.inID.Name = "inID"
-        Me.inID.Size = New System.Drawing.Size(100, 22)
+        Me.inID.Size = New System.Drawing.Size(257, 30)
         Me.inID.TabIndex = 2
         '
         'inname
         '
-        Me.inname.Location = New System.Drawing.Point(67, 95)
+        Me.inname.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.inname.Location = New System.Drawing.Point(109, 103)
         Me.inname.Name = "inname"
-        Me.inname.Size = New System.Drawing.Size(100, 22)
+        Me.inname.Size = New System.Drawing.Size(257, 30)
         Me.inname.TabIndex = 3
         '
         'btn1
         '
-        Me.btn1.Location = New System.Drawing.Point(67, 205)
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(109, 211)
         Me.btn1.Name = "btn1"
-        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.Size = New System.Drawing.Size(129, 58)
         Me.btn1.TabIndex = 4
         Me.btn1.Text = "Button1"
         Me.btn1.UseVisualStyleBackColor = True
         '
         'inprice
         '
-        Me.inprice.Location = New System.Drawing.Point(67, 133)
+        Me.inprice.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.inprice.Location = New System.Drawing.Point(109, 139)
         Me.inprice.Name = "inprice"
-        Me.inprice.Size = New System.Drawing.Size(100, 22)
+        Me.inprice.Size = New System.Drawing.Size(257, 30)
         Me.inprice.TabIndex = 5
         '
         'DateTimeIncome
         '
-        Me.DateTimeIncome.Location = New System.Drawing.Point(67, 162)
+        Me.DateTimeIncome.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimeIncome.Location = New System.Drawing.Point(109, 175)
         Me.DateTimeIncome.Name = "DateTimeIncome"
-        Me.DateTimeIncome.Size = New System.Drawing.Size(203, 22)
+        Me.DateTimeIncome.Size = New System.Drawing.Size(257, 30)
         Me.DateTimeIncome.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(-1, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 25)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(-1, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 25)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(-1, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 25)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(-1, 175)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 25)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Label4"
         '
         'Form5
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1269, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.DateTimeIncome)
         Me.Controls.Add(Me.inprice)
         Me.Controls.Add(Me.btn1)
@@ -87,4 +140,8 @@ Partial Class Form5
     Friend WithEvents btn1 As Button
     Friend WithEvents inprice As TextBox
     Friend WithEvents DateTimeIncome As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
