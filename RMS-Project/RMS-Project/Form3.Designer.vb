8ecf438d99ba88dde08ae2437965ddee7ed6e59c
Puttipong Nuphan <teedome35@gmail.com>
2020-04-24 12:57:32 +0700

    แก้

--- RMS-Project/RMS-Project/Form3.Designer.vb
+++ RMS-Project/RMS-Project/Form3.Designer.vb
@@ -23,239 +23,72 @@ Partial Class Form3
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Me.table = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.DataSet1 = New RMS_Project.DataSet1()
-        Me.DataTable1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.DataTable1TableAdapter = New RMS_Project.DataSet1TableAdapters.DataTable1TableAdapter()
-        Me.Menu = New RMS_Project.menu()
-        Me.MenuBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.MenuBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-        Me.FoodBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.FoodTableAdapter = New RMS_Project.menuTableAdapters.foodTableAdapter()
-        Me.FoodidDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.FoodnameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.FoodpriceDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Table = New RMS_Project.table()
-        Me.TableBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.TableTableAdapter = New RMS_Project.tableTableAdapters.TableTableAdapter()
         Me.Table1 = New RMS_Project.table()
-        Me.TableBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataTable1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Menu, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.MenuBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.MenuBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.FoodBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Table, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.DataTable1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DataTable1TableAdapter = New RMS_Project.tableTableAdapters.DataTable1TableAdapter()
         CType(Me.Table1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TableBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataTable1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'table
+        '
+        Me.table.AutoSize = True
+        Me.table.BackColor = System.Drawing.SystemColors.Highlight
+        Me.table.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.table.Location = New System.Drawing.Point(53, 78)
+        Me.table.Name = "table"
+        Me.table.Size = New System.Drawing.Size(89, 17)
+        Me.table.TabIndex = 0
+        Me.table.Text = "เลือกโต๊ะอาหาร"
+        Me.table.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.Yellow
-        Me.Label1.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(361, 76)
+        Me.Label1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(304, 78)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(168, 29)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "รายการอาหาร"
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.AutoGenerateColumns = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.FoodidDataGridViewTextBoxColumn, Me.FoodnameDataGridViewTextBoxColumn, Me.FoodpriceDataGridViewTextBoxColumn})
-        Me.DataGridView1.DataSource = Me.FoodBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(321, 138)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(345, 342)
-        Me.DataGridView1.TabIndex = 1
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.BackColor = System.Drawing.Color.Yellow
-        Me.Label2.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(746, 76)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(212, 29)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "รายการที่สั่ง/ราคา"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.BackColor = System.Drawing.Color.Yellow
-        Me.Label3.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(53, 76)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(178, 29)
-        Me.Label3.TabIndex = 3
-        Me.Label3.Text = "เลือกโต๊ะอาหาร"
-        '
-        'Button1
-        '
-        Me.Button1.BackColor = System.Drawing.Color.Yellow
-        Me.Button1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(781, 457)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(143, 44)
-        Me.Button1.TabIndex = 5
-        Me.Button1.Text = "สั่งอาหาร"
-        Me.Button1.UseVisualStyleBackColor = False
-        '
-        'DataSet1
-        '
-        Me.DataSet1.DataSetName = "DataSet1"
-        Me.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'DataTable1BindingSource
-        '
-        Me.DataTable1BindingSource.DataMember = "DataTable1"
-        Me.DataTable1BindingSource.DataSource = Me.DataSet1
-        '
-        'DataTable1TableAdapter
-        '
-        Me.DataTable1TableAdapter.ClearBeforeFill = True
-        '
-        'Menu
-        '
-        Me.Menu.DataSetName = "menu"
-        Me.Menu.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'MenuBindingSource
-        '
-        Me.MenuBindingSource.DataSource = Me.Menu
-        Me.MenuBindingSource.Position = 0
-        '
-        'MenuBindingSource1
-        '
-        Me.MenuBindingSource1.DataSource = Me.Menu
-        Me.MenuBindingSource1.Position = 0
-        '
-        'FoodBindingSource
-        '
-        Me.FoodBindingSource.DataMember = "food"
-        Me.FoodBindingSource.DataSource = Me.Menu
-        '
-        'FoodTableAdapter
-        '
-        Me.FoodTableAdapter.ClearBeforeFill = True
-        '
-        'FoodidDataGridViewTextBoxColumn
-        '
-        Me.FoodidDataGridViewTextBoxColumn.DataPropertyName = "food_id"
-        Me.FoodidDataGridViewTextBoxColumn.HeaderText = "food_id"
-        Me.FoodidDataGridViewTextBoxColumn.Name = "FoodidDataGridViewTextBoxColumn"
-        '
-        'FoodnameDataGridViewTextBoxColumn
-        '
-        Me.FoodnameDataGridViewTextBoxColumn.DataPropertyName = "food_name"
-        Me.FoodnameDataGridViewTextBoxColumn.HeaderText = "food_name"
-        Me.FoodnameDataGridViewTextBoxColumn.Name = "FoodnameDataGridViewTextBoxColumn"
-        '
-        'FoodpriceDataGridViewTextBoxColumn
-        '
-        Me.FoodpriceDataGridViewTextBoxColumn.DataPropertyName = "food_price"
-        Me.FoodpriceDataGridViewTextBoxColumn.HeaderText = "food_price"
-        Me.FoodpriceDataGridViewTextBoxColumn.Name = "FoodpriceDataGridViewTextBoxColumn"
-        '
-        'Table
-        '
-        Me.Table.DataSetName = "table"
-        Me.Table.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'TableBindingSource
-        '
-        Me.TableBindingSource.DataMember = "Table"
-        Me.TableBindingSource.DataSource = Me.Table
-        '
-        'TableTableAdapter
-        '
-        Me.TableTableAdapter.ClearBeforeFill = True
+        Me.Label1.Size = New System.Drawing.Size(124, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "เลือกเมนูอาหารที่จะสั่ง"
         '
         'Table1
         '
         Me.Table1.DataSetName = "table"
         Me.Table1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
-        'TableBindingSource1
+        'DataTable1BindingSource
         '
-        Me.TableBindingSource1.DataMember = "Table"
-        Me.TableBindingSource1.DataSource = Me.Table1
+        Me.DataTable1BindingSource.DataMember = "DataTable1"
+        Me.DataTable1BindingSource.DataSource = Me.Table1
         '
-        'ComboBox1
+        'DataTable1TableAdapter
         '
-        Me.ComboBox1.DataBindings.Add(New System.Windows.Forms.Binding("SelectedValue", Me.TableBindingSource, "table_number", True))
-        Me.ComboBox1.DataSource = Me.TableBindingSource1
-        Me.ComboBox1.DisplayMember = "table_number"
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(48, 151)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(183, 24)
-        Me.ComboBox1.TabIndex = 4
-        Me.ComboBox1.ValueMember = "table_number"
+        Me.DataTable1TableAdapter.ClearBeforeFill = True
         '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.PaleTurquoise
-        Me.ClientSize = New System.Drawing.Size(1181, 539)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(917, 534)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.table)
         Me.Name = "Form3"
         Me.Text = "Form3"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataTable1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Menu, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.MenuBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.MenuBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.FoodBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Table, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Table1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TableBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataTable1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents table As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents DataSet1 As DataSet1
-    Friend WithEvents DataTable1BindingSource As BindingSource
-    Friend WithEvents DataTable1TableAdapter As DataSet1TableAdapters.DataTable1TableAdapter
-    Friend WithEvents MenuBindingSource1 As BindingSource
-    Friend WithEvents Menu As menu
-    Friend WithEvents MenuBindingSource As BindingSource
-    Friend WithEvents FoodBindingSource As BindingSource
-    Friend WithEvents FoodTableAdapter As menuTableAdapters.foodTableAdapter
-    Friend WithEvents FoodidDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents FoodnameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents FoodpriceDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents Table As table
-    Friend WithEvents TableBindingSource As BindingSource
-    Friend WithEvents TableTableAdapter As tableTableAdapters.TableTableAdapter
     Friend WithEvents Table1 As table
-    Friend WithEvents TableBindingSource1 As BindingSource
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents DataTable1BindingSource As BindingSource
+    Friend WithEvents DataTable1TableAdapter As tableTableAdapters.DataTable1TableAdapter
 End Class
--- RMS-Project/RMS-Project/Form3.vb
+++ RMS-Project/RMS-Project/Form3.vb
@@ -1,13 +1,7 @@
 ﻿Public Class Form3
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'Table1._Table' table. You can move, or remove it, as needed.
-        Me.TableTableAdapter.Fill(Me.Table1._Table)
-        'TODO: This line of code loads data into the 'Table._Table' table. You can move, or remove it, as needed.
-        Me.TableTableAdapter.Fill(Me.Table._Table)
-        'TODO: This line of code loads data into the 'Menu.food' table. You can move, or remove it, as needed.
-        Me.FoodTableAdapter.Fill(Me.Menu.food)
-        'TODO: This line of code loads data into the 'DataSet1.DataTable1' table. You can move, or remove it, as needed.
-        Me.DataTable1TableAdapter.Fill(Me.DataSet1.DataTable1)
+        'TODO: This line of code loads data into the 'Table1.DataTable1' table. You can move, or remove it, as needed.
+        Me.DataTable1TableAdapter.Fill(Me.Table1.DataTable1)
 
     End Sub
 End Class
