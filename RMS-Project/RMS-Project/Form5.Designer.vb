61480a1fa3bc1f1e537522c4c6318beaff29dda4
nung3044 <neungunlove@hotmail.com>
2020-04-25 15:52:54 +0700

    db

--- RMS-Project/RMS-Project/Form5.Designer.vb
+++ RMS-Project/RMS-Project/Form5.Designer.vb
@@ -147,7 +147,7 @@ Partial Class Form5
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IncomeIDDataGridViewTextBoxColumn, Me.IncomeNameDataGridViewTextBoxColumn, Me.IncomePriceDataGridViewTextBoxColumn, Me.IncomeDateDataGridViewTextBoxColumn})
         Me.DataGridView1.DataSource = Me.IncomeBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(411, 40)
+        Me.DataGridView1.Location = New System.Drawing.Point(393, 40)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 51
         Me.DataGridView1.RowTemplate.Height = 24
@@ -295,7 +295,7 @@ Partial Class Form5
         'TextBox1
         '
         Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(130, 378)
+        Me.TextBox1.Location = New System.Drawing.Point(88, 334)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(257, 30)
         Me.TextBox1.TabIndex = 17
