0a9d6444f42406f8d98ea2290f7d73af34bbd4fb
NANTANAT JUNTALERT <6110210211@psu.ac.th>
2020-04-26 00:54:57 +0700

    Revert "อัพของโดม"
    This reverts commit 2c28a167c2c1c677f129a52192cf29df2d6405ee.

--- RMS-Project/RMS-Project/Form3.vb
+++ RMS-Project/RMS-Project/Form3.vb
@@ -1,8 +1,4 @@
-﻿Imports System.Data.SqlClient
-Public Class b
-
-    Dim con As New SqlConnection
-    Dim cmd As New SqlCommand
+﻿Public Class b
 
     Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
 
@@ -13,54 +9,14 @@ Public Class b
         Me.ShoptableTableAdapter.Fill(Me.Table.shoptable)
         'TODO: This line of code loads data into the 'Food._food' table. You can move, or remove it, as needed.
         Me.FoodTableAdapter.Fill(Me.Food._food)
-        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\COVID-19-Organization\MiniProject\RMS-Project\RMS-Project\Shop.mdf;Integrated Security=True"
-        If con.State = ConnectionState.Open Then
-            con.Close()
-        End If
-        con.Open()
-        disp_data()
-
-    End Sub
-    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles submit.Click
-        cmd = con.CreateCommand()
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = "insert into income values('" & id.Text & "','" & DateTimeIncome.Value & "','" & cbbTable.Text & "','" & TextBox1.Text & "','" & TextBox2.Text & "','" & Num1.Text & "')"
-        cmd.ExecuteNonQuery()
-        disp_data()
-
-        MessageBox.Show("เพิ่มข้อมูลสำเร็จ")
-
 
     End Sub
 
-    Public Sub disp_data()
-
-        cmd = con.CreateCommand()
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = "Select * from order"
-        cmd.ExecuteNonQuery()
-
-
-        Dim colsum As Decimal
-
-        For Each R As DataGridViewRow In DataGridView1.Rows
-
-            colsum += R.Cells(2).Value
-
-        Next
-        sum1.Text = colsum
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub DataGridView1_CellMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles DataGridView1.CellMouseClick
-
-        Dim i As Integer
-        i = DataGridView1.CurrentRow.Index
-
-        TextBox1.Text = DataGridView1.Item(0, i).Value
-        TextBox2.Text = DataGridView1.Item(1, i).Value
-
+    Private Sub cbbTable_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbbTable.SelectedIndexChanged
 
     End Sub
-
 End Class
