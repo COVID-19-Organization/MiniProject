ca65ad37c0099c44865c61209f6a6fff5bf18cda
nung3044 <neungunlove@hotmail.com>
2020-04-25 15:44:07 +0700

    test

--- RMS-Project/RMS-Project/Form5.Designer.vb
+++ RMS-Project/RMS-Project/Form5.Designer.vb
@@ -147,7 +147,7 @@ Partial Class Form5
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IncomeIDDataGridViewTextBoxColumn, Me.IncomeNameDataGridViewTextBoxColumn, Me.IncomePriceDataGridViewTextBoxColumn, Me.IncomeDateDataGridViewTextBoxColumn})
         Me.DataGridView1.DataSource = Me.IncomeBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(393, 40)
+        Me.DataGridView1.Location = New System.Drawing.Point(411, 40)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 51
         Me.DataGridView1.RowTemplate.Height = 24
